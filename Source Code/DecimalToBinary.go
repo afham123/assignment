c5b52db14e531ee18ec9ea6b2f825245a8b7be05
Afham Fardeen <49563140+afham123@users.noreply.github.com>
2021-10-12 19:21:44 +0530

    Update DecimalToBinary.go

--- Source Code/DecimalToBinary.go
+++ Source Code/DecimalToBinary.go
@@ -1,7 +1,14 @@
 package main
 import "fmt"
+import "strconv"
 
 func main(){
-    n := int64(12)
-    fmt.Printf("%b", n)
+    n := 12
+    var res = ""
+    for (n>0){
+        r := (n%2)
+        res=strconv.Itoa(r)+res
+        n/=2
+    }
+    fmt.Printf(res)
 }
