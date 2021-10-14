93f1b2edcd339efdcc648754a3c21e3bd6c13d4b
Afham Fardeen <49563140+afham123@users.noreply.github.com>
2021-10-14 11:05:55 +0530

    Update main.go

--- Code/Main/main.go
+++ Code/Main/main.go
@@ -9,8 +9,8 @@ import (
 	"encoding/base64"
 )
 
-var keyValue map[string]string
-var keyHash map[string]string
+var keyValue map[string]string   // Hash map to store key(dates in string) and its news content(in string type)
+var keyHash map[string]string    // Hash map to store key(date in string) and its hash value(in string type).
 
 
 //Function that takes key, value pair as an argument and 
@@ -34,11 +34,18 @@ func Put(key string, value string){
     keyHash[key] = sha  // storing key hash in keyHash hash map 
 }
 
+// Function to return actual news and its Hash Value
+func Get(key string)string{
+    return keyValue[key],keyHash[key]
+}
+
+
 func main() {
   
   
   reader := bufio.NewReader(os.Stdin)
-  fmt.Println("Enter the number of key value pair to store in kv store")
+  
+  // Value to enter the number of key value pair to store in kv store
   num := readNum(reader)
   
   for i:=1;i<=num;i++{
@@ -51,6 +58,10 @@ func main() {
     
     fmt.Println(keyValue)
     fmt.Println(keyHash)
+    
+    
+    key := readString(reader)     // Enter the key(date) that user want to see.
+    news,hash = Get(key)
 }
 
 // Reading input functions
